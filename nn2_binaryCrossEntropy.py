f6f9137ea0a1307036c4c31455b2bf2f844984d6
Kaushik Srivatsan <kaushiksrivatsan2003@hotmail.com>
2024-07-24 12:39:03 +0530

    Commit cross entropy

--- nn2_binaryCrossEntropy.py
+++ nn2_binaryCrossEntropy.py
@@ -4,9 +4,6 @@ from nnfs.datasets import spiral_data
 nnfs.init()
 
 
-
-X,y = spiral_data(samples = 100, classes = 3)
-
 class Layer_Dense:
     def __init__(self,n_inputs,n_neurons, weight_regularizer_l1=0, weight_regularizer_l2=0, bias_regularizer_l1=0, bias_regularizer_l2=0):
         self.weights = 0.10 * np.random.randn(n_inputs,n_neurons)
@@ -83,6 +80,15 @@ class Activation_Softmax:
             #calculate sample-wise gradient
             #and add it to the array of sample gradients
             self.dinputs[index] = np.dot(jacobian_matrix,single_dvalues)
+
+class Activation_Sigmoid:
+    def forward(self, inputs):
+        self.inputs = inputs
+        self.output = 1 / (1 + np.exp(-inputs))
+
+    def backward(self,dvalues):
+        self.dinputs = dvalues * (1 - self.output) * self.output
+
 
 class Optimizer_SGD:
     def __init__(self, learning_rate=1., decay=0.,momentum=0.):
@@ -282,68 +288,83 @@ class Activation_Softmax_Loss_CategoricalCrossentropy():
         self.dinputs[range(samples),y_true] -= 1
         self.dinputs = self.dinputs / samples
 
-X,y = spiral_data(samples = 100, classes = 3)
+class Loss_BinaryCrossentropy(Loss):
+    def forward(self, y_pred, y_true):
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1-1e-7)
+        sample_losses = -(y_true * np.log(y_pred_clipped) + (1-y_true) * np.log(1-y_pred_clipped))
+        sample_losses = np.mean(sample_losses, axis = -1)
+        return sample_losses
+    
+    def backward(self,dvalues, y_true):
+        #number of samples
+        samples = len(dvalues)
+        #number of outputs in every sample, using first sample to count them
+        outputs = len(dvalues[0])
+        clipped_dvalues = np.clip(dvalues, 1e-7, 1-1e-7)
+        self.dinputs = -(y_true / clipped_dvalues - (1-y_true) / (1-clipped_dvalues)) / outputs
+        self.dinputs = self.dinputs / samples
 
+
+X,y = spiral_data(samples = 100, classes = 2)
+
+y = y.reshape(-1,1) # [samples,] -> [samples,1] eg. [1,2,3] -> [[1],[2],[3]]
 
 # Create dense layer with 2 input features and 64 output values
 dense1 = Layer_Dense(2,64, weight_regularizer_l2=5e-4, bias_regularizer_l2=5e-4)
 activation1 = Activation_ReLU()
-dropout1 = Layer_Dropout(0.1)
 
 #Create second dense layer with 64 input features  and 3 output values
 dense2 = Layer_Dense(64,3)
+activation2 = Activation_Sigmoid()
 
-loss_activation = Activation_Softmax_Loss_CategoricalCrossentropy()
+loss_function = Loss_BinaryCrossentropy()
 
 optimizer = Optimizer_Adam(learning_rate = 0.05, decay=5e-7)
 
 for epoch in range(10001):
     dense1.forward(X)
     activation1.forward(dense1.output)
-    dropout1.forward(activation1.output)
 
     dense2.forward(activation1.output)
 
     #Perform a forward pass of the combined activation function and loss function
     #takes the output of second dense layer here and the true labels
-    data_loss = loss_activation.forward(dense2.output,y)
+    activation2.forward(dense2.output)
+    data_loss = loss_function.calculate(activation2.output,y)   
 
-    regularization_loss = loss_activation.loss.regularization_loss(dense1)  + loss_activation.loss.regularization_loss(dense2)
+    regularization_loss = loss_function.loss.regularization_loss(dense1)  + loss_function.loss.regularization_loss(dense2)
     loss = data_loss + regularization_loss
 
-    #loss_activation.output is the output from the softmax activation function  
-    predictions = np.argmax(loss_activation.output, axis = 1)
-    if len(y.shape) == 2:
-         y = np.argmax(y, axis = 1)
+    predictions = (activation2.output > 0.5) * 1
     accuracy = np.mean(predictions == y)
 
     if not epoch % 100:
         print(f'epoch: {epoch}, ' + f'acc: {accuracy:.3f}, ' + f'loss: {loss:.3f}')
 
 
-#Backward pass
-loss_activation.backward(loss_activation.output,y)
-dense2.backward(loss_activation.dinputs)
-dropout1.backward(dense2.dinputs)
-activation1.backward(dense2.dinputs)
-dense1.backward(activation1.dinputs)
+    #Backward pass
+    loss_function.backward(loss_function.output,y)
+    activation2.backward(loss_function.dinputs)
+    dense2.backward(activation2.dinputs)
+    activation1.backward(dense2.dinputs)
+    dense1.backward(activation1.dinputs)
 
-#Update weights and biases
-optimizer.pre_update_params()
-optimizer.update_params(dense1)
-optimizer.update_params(dense2)
-optimizer.post_update_params()
+    #Update weights and biases
+    optimizer.pre_update_params()
+    optimizer.update_params(dense1)
+    optimizer.update_params(dense2)
+    optimizer.post_update_params()
 
 #Validated the model
-X_test, y_test = spiral_data(samples = 100, classes = 3)
+X_test, y_test = spiral_data(samples = 100, classes = 2)
+y_test = y_test.reshape(-1,1)
 dense1.forward(X_test)
 activation1.forward(dense1.output)
 dense2.forward(activation1.output)
-loss = loss_activation.forward(dense2.output,y_test)
+activation2.forward(dense2.output)
+loss = loss_function.calculate(activation2.output,y_test)
 
-predictions = np.argmax(loss_activation.output, axis = 1)
-if len(y_test.shape) == 2:
-    y_test = np.argmax(y_test, axis = 1)
+predictions = (activation2.output > 0.5) * 1
 accuracy = np.mean(predictions == y_test)
 
 print(f'Validation, acc: {accuracy:.3f}, loss: {loss:.3f}')
